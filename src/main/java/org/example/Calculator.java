e3e27355dc2e2ab9a661b0548472765ccb2813f0
Constanta <konstantinova-2002@mail.ru>
2022-12-22 02:05:59 +0300

    init proj

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double sharing(int a, int b) {
+        if (b == 0) {
+            return -100;
+        } else {
+            return a / b;
+        }
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,24 @@
+import org.example.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    Calculator calculator= new Calculator();
+    @Test
+    void Addition ()
+    {
+        assertEquals(3, calculator.add(1,2));
+    }
+    @Test
+    void subtraction()
+    {
+        assertEquals(5, calculator.subtract(7,2));
+    }
+    @Test
+    void multiplition(){
+        assertEquals(40, calculator.multiply(5, 8));
+    }
+    @Test
+    void share(){assertEquals(6, calculator.sharing(42,7));};
+}
